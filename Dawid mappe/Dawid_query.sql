fad7514a79098919833b753d86a48b045b2200d1
Ixtrialos <jakkdk@gmail.com>
2026-03-06 11:22:51 +0100

    Recomitter Dawids fil så han ikke piver mere

---
+++ Dawid mappe/Dawid_query.sql
@@ -0,0 +1,22 @@
+-- Laver lige drinks alter
+
+CREATE TABLE drink(
+drink_id int not null, 
+navn char(25) not null,
+mælk_forbrug int not null, 
+vand_forbrug int not null, 
+
+primary key(drink_id)
+);
+
+CREATE TABLE ansat(
+medarbejder_id int not null,
+navn		char(25) not null,
+efternavn	char(25) not null,
+stilling 	char(25) not null,	
+brugernavn 	char(25) not null,
+kodeord 	char(25) not null,
+adgangstilladelse bool not null,
+primary key(medarbejder_id)
+);
+
