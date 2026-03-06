a04083080c69e6e637834971b9eec01c7015f89a
Dave the Magical cheese wizzard <klasadawid@hotmail.com>
2026-03-06 11:24:09 +0100

    hvad sker

--- Dawid mappe/Dawid_query.sql
+++ Dawid mappe/Dawid_query.sql
@@ -5,7 +5,6 @@ drink_id int not null,
 navn char(25) not null,
 mælk_forbrug int not null, 
 vand_forbrug int not null, 
-
 primary key(drink_id)
 );
 
@@ -19,4 +18,28 @@ kodeord 	char(25) not null,
 adgangstilladelse bool not null,
 primary key(medarbejder_id)
 );
+
+
+CREATE TABLE ansat(
+medarbejder_id int not null,
+navn		char(25) not null,
+efternavn	char(25) not null,
+stilling 	char(25) not null,	
+brugernavn 	char(25) not null,
+kodeord 	char(25) not null,
+adgangstilladelse bool not null,
+primary key(medarbejder_id)
+);
+
+
+CREATE TABLE rengøring(
+rengøring_id 		int not null,
+medarbejder_id		char(25) not null,
+dato	 			date not null,
+primary key(medarbejder_id)
+);
+
+
+
+
 
