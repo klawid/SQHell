ce380c29c9b3ef5e56b0cea7d25f8aea2d65a666
Dave the Magical cheese wizzard <klasadawid@hotmail.com>
2026-04-26 11:41:25 +0200

    Fjernede lige gyldige kommentar

--- Vores mappe/kaffe_queries.sql
+++ Vores mappe/kaffe_queries.sql
@@ -1,41 +1,4 @@
 
-
-
--- 		NEMME/TRÆNINGS OPGVAER 
-
--- Vis navn på medarbejdere 
-
-SELECT medarbejder_id, navn,efternavn 
-FROM ansat; 
-
--- Find lager historik 
-	-- Code
-
--- Get lager status (hvad er der på nuværende tidspunkt?) 
-	-- Code
-    
-
-
-
--- MUST/MERE KOMPLICEREDE OPGAVER
-
--- Liste af alle transaktioner: Alle transaktioner med en bestemt drink
-
-SELECT transakion_id, ansat. navn, dato, tidspunkt, drink 
-
--- Liste af alle transaktioner: Alle transaktioner indenfor et bestemt tidsrum (samme dag? Over flere dage) 
-
--- Daglig forbrug 
-		-- Code
-        
--- (ikke helt denne, men god at have med) Liste af alle transaktioner: Alle transaktioner indenfor af en bestemt person 
-
-
-
-
-USE kaffemaskine;
-
-
 -- ---------------------------------------------------------------------
 -- QUERY 1: Transaktioner — filtrerbar efter tidsrum, drink, betalingstype
 -- ---------------------------------------------------------------------
@@ -134,7 +97,6 @@ SELECT antal_200kr, antal_100kr, antal_50kr, antal_20kr,
 -- ---------------------------------------------------------------------
 -- QUERY 3: Rengøringshistorik
 -- ---------------------------------------------------------------------
-
 SELECT r.rengøring_id,
        r.dato,
        CONCAT(a.navn, ' ', a.efternavn) AS udført_af,
