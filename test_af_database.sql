698e2e4bd1f469fa9571f9cbb9178e3cf13b0c27
Dave the Magical cheese wizzard <klasadawid@hotmail.com>
2026-04-27 10:27:51 +0200

    Ændring af filnavn + basic fix i injecting

--- test_af_database.sql
+++ test_af_database.sql
@@ -206,3 +206,73 @@ SELECT antal_200kr, antal_100kr, antal_50kr, antal_20kr, antal_10kr, antal_5kr, 
 
 
 
+
+
+
+-- =====================================================================
+-- Test af: TRIGGER Transaktion
+-- =====================================================================
+
+-- Anvendelse af testen:  
+	-- 1: Update lager til en bestemt/vilkårig værdi 
+    -- 2: Prøv at insert en test værdi. Med givet værdier, vil error "ikke nok kaffe på lager" vises. Dette sker pga. TRIGGER. 
+		-- Brug select 
+    -- 3: UPDATE lager med givet værdier.  
+    -- 4: Select der visser før og efter lager værdi. Vil ikke virke hvis forrige select ikke blev kørt 
+    -- 5: Udfør test inserten igen, der denne gang vil virke.
+    -- 6: Compile af denne select vil visse "efter_kaffe" med nye værdier pga. TRIGGER, der automatisk har opdateret lager. Her vil ny værdi og kaffe- samt mælk kosten for drunk 2 kunne ses 
+
+UPDATE lager						-- 1: Update lager til en vilkårig værdi
+SET 
+    mængde_kaffe = 0,
+    mængde_mælk = 0
+WHERE lager_id = 1;
+
+INSERT INTO transaktion VALUES		-- 2: Prøv at insert en test værdi.
+(99,  1, 2, 1,  0,  0, 0, '2026-02-05', '05:06:51');
+
+SELECT 											-- Brug select 
+    mængde_kaffe, 
+    mængde_mælk
+INTO @for_kaffe, @for_melk			-- Lige denne kode kan ikke tage æ og ø
+FROM lager
+WHERE lager_id = 1;
+
+
+UPDATE lager						-- 3: UPDATE lager med givet værdier.  
+SET 
+    mængde_kaffe = 1000,
+    mængde_mælk = 200
+WHERE lager_id = 1;
+
+
+SELECT								-- 4: Vis før og efter lager værdi med denne select. Vil ikke virke hvis forrige select ikke blev kørt  								
+    @for_kaffe	 AS kaffe_før_update,
+    mængde_kaffe AS efter_kaffe,
+    @for_melk AS mælk_før_update,
+    mængde_mælk AS efter_mælk
+FROM lager
+WHERE lager_id = 1;
+
+INSERT INTO transaktion VALUES    -- 5: Udfør test inserten igen, der denne gang vil virke.				
+(99,  1, 2, 1,  0,  0, 0, '2026-02-05', '05:06:51');
+
+
+SELECT								-- 6: Compile af denne select vil visse "efter_kaffe" med nye værdier pga. TRIGGER, der automatisk har opdateret lager. Her vil ny værdi og kaffe- samt mælk kosten for drunk 2 kunne ses 								
+    @for_kaffe	 AS kaffe_før_update,
+    mængde_kaffe AS efter_kaffe,
+    @for_melk AS mælk_før_update,
+    mængde_mælk AS efter_mælk,
+	kaffe_forbrug_g,
+	mælk_forbrug_ml
+FROM lager
+ JOIN drink ON drink_id = 2
+WHERE lager_id = 1;
+
+
+
+-- =====================================================================
+-- Test af: PROCEDURE køb_drink
+-- =====================================================================
+
+
